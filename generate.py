2fc00bab42caefc71d496c6a4e5c3b86232b1bc4
Scott Harrison <scottrules44@gmail.com>
2023-02-21 20:22:39 -0600

    Adding Ids for sharing links

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import json
 import sys
-
+import uuid
 
 if __name__ == "__main__":
     repos = {}
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     with open("docs/code.json", "r", encoding='utf-8') as f:
         repos = json.load(f)
 
-    repos['data'].insert(0, {"title":title, "description":description, "repo":codeRepoLink})
+    randomId = uuid.uuid4().hex[:4] #assign a random number for sharing
+    repos['data'].insert(0, {"title":title, "description":description, "repo":codeRepoLink, "id":randomId})
     with open('docs/code.json', 'w') as f:
-        f.write(json.dumps(repos))
+        f.write(json.dumps(repos, sort_keys=True, indent=0))
